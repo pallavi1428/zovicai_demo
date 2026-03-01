2400fc0fba0a1a3ba5af9faf37974cc8a5ff3689
unknown <helloiampallavi@gmail.com>
2026-03-01 16:04:45 +0530

    make changes in cn

--- zdemo/shared/utils/cn.ts
+++ zdemo/shared/utils/cn.ts
@@ -1,3 +1,18 @@
-export function cn(...classes: (string | boolean | undefined | null)[]) {
-  return classes.filter(Boolean).join(' ');
+export function cn(
+  ...classes: (string | boolean | undefined | null | Record<string, boolean>)[]
+) {
+  return classes
+    .map((cls) => {
+      if (!cls) return '';
+      if (typeof cls === 'string') return cls;
+      if (typeof cls === 'object') {
+        return Object.entries(cls)
+          .filter(([_, value]) => value)
+          .map(([key]) => key)
+          .join(' ');
+      }
+      return '';
+    })
+    .filter(Boolean)
+    .join(' ');
 }
