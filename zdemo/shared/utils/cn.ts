1f56bc2b9e3ed3ad0b21620c4e2332bd2641019e
unknown <helloiampallavi@gmail.com>
2026-03-01 16:25:33 +0530

    cn is too strict

--- zdemo/shared/utils/cn.ts
+++ zdemo/shared/utils/cn.ts
@@ -1,18 +1,6 @@
-export function cn(
-  ...classes: (string | boolean | undefined | null | Record<string, boolean>)[]
-) {
-  return classes
-    .map((cls) => {
-      if (!cls) return '';
-      if (typeof cls === 'string') return cls;
-      if (typeof cls === 'object') {
-        return Object.entries(cls)
-          .filter(([_, value]) => value)
-          .map(([key]) => key)
-          .join(' ');
-      }
-      return '';
-    })
-    .filter(Boolean)
-    .join(' ');
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
